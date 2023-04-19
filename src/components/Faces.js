4aa49a8df03b124b15a79e33ec749b23a84eaeae
badrumour <sanjeetkshirasagar10@gmail.com>
2023-04-19 16:12:58 +0530

    final updat

--- src/components/Faces.js
+++ src/components/Faces.js
@@ -1,13 +1,33 @@
 import React from "react";
 import "./Faces.css";
+import { useState, useEffect } from "react";
 
 const Faces = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch("https://reqres.in/api/users?page=2");
+        const json = await response.json();
+        setUsers(json.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchUsers();
+  }, []);
+
   return (
     <div className="faces">
-      <div className="faces-center">
-      
-      
-      </div>
+      {users.map((user) => (
+        <div key={user.id} className="Movie">
+          <p>
+            {user.first_name} {user.last_name}
+          </p>
+          <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+        </div>
+      ))}
     </div>
   );
 };
