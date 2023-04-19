1b74d52a4270b3847ba0f30625cd73fc6233480b
badrumour <sanjeetkshirasagar10@gmail.com>
2023-04-19 09:38:29 +0530

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+}
+
+export default NavBar;
